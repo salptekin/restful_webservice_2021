43ab12e57e516015578edbe0e670ebaa66007af7
apple <apple@apples-imac-6.attlocal.net>
2021-07-06 20:47:11 -0400

    third commit

--- src/main/java/restfulwebservice03/SF03StudentBeanService.java
+++ src/main/java/restfulwebservice03/SF03StudentBeanService.java
@@ -1,6 +1,11 @@
 package restfulwebservice03;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +31,9 @@ public class SF03StudentBeanService {
 			return new SF03StudentBean();	
 		}
 	}
-	
+	//For invalid ids, the method will throw IllegalStateException with "id does not exist" message
+	//For valid ids, student with the id will be removed from the database and you will get a success
+	//message on the console like "Student whose id is 'id' is successfully deleted"
 	public String deleteStdById(Long id) {
 		if(!studentRepo.existsById(id)) {
 			throw new IllegalStateException(id + " does not exist");
@@ -35,22 +42,41 @@ public class SF03StudentBeanService {
 			return "Student whose id is " + id + " is successfully deleted";	
 	}
 	
-	public SF03StudentBean updateStudent(Long id, SF03StudentBean student) {
+	//The method is for fully update
+	public SF03StudentBean updateStudent(Long id, SF03StudentBean newStudent) {
 		
-		SF03StudentBean existingStudent = studentRepo
+		SF03StudentBean existingStudentById = studentRepo
 												.findById(id)
-												.orElseThrow(()->new IllegalStateException("Does not exist.."));
+												.orElseThrow(()->new IllegalStateException(id + " id does not exist.."));
 		
-		String name = existingStudent.getName();
-		if(student.getName()==null) {
-			existingStudent.setName(null);
-		}else if(existingStudent.getName()==null) {
-			existingStudent.setName(student.getName());
-		}else if(!name.equals(student.getName())) {
-			existingStudent.setName(student.getName());
+		String name = existingStudentById.getName();		
+		if(newStudent.getName()==null) {			
+			existingStudentById.setName(null);			
+		}else if(existingStudentById.getName()==null) {			
+			existingStudentById.setName(newStudent.getName());			
+		}else if(!name.equals(newStudent.getName())) {			
+			existingStudentById.setName(newStudent.getName());			
 		}
 		
-		return studentRepo.save(student);
+		Optional<SF03StudentBean> existingStudentByEmail = studentRepo.findSF03StudentBeanByEmail(newStudent.getEmail());
+		
+		if(existingStudentByEmail.isPresent()) {			
+			throw new IllegalStateException("Email is taken, cannot be used again...");			
+		}else if(newStudent.getEmail()==null) {			
+			throw new IllegalArgumentException("Email must be sent...");			
+		}else if(!newStudent.getEmail().contains("@")) {			
+			throw new IllegalArgumentException("Invalid email id is used, fix it...");			
+		}else if(!existingStudentById.getEmail().equals(newStudent.getEmail())){			
+			existingStudentById.setEmail(newStudent.getEmail());			
+		}
+		
+		if(Period.between(newStudent.getDob(), LocalDate.now()).isNegative()) {
+			throw new IllegalStateException("Date of birth cannot be selected from future...");
+		}else if(!existingStudentById.getDob().equals(newStudent.getDob())) {
+			existingStudentById.setDob(newStudent.getDob());
+		}
+		
+		return studentRepo.save(newStudent);
 		
 	}
 	
